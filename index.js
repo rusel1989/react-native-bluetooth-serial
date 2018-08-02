29d2d7a540328003469dc08599d27f221339d6c0
Ihor Kulikovskyi <ihor.kulikovskyi@eleks.com>
2018-08-02 17:48:43 +0300

    Updated index.js

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const ReactNative = require('react-native')
 const { Buffer } = require('buffer')
-const { NativeModules, DeviceEventEmitter } = ReactNative
+const { NativeModules, DeviceEventEmitter } = ReactNative;
 const BluetoothSerial = NativeModules.BluetoothSerial
 
+const listeners = {};
+
+const removeListenerByName = function(eventName)  {
+  listeners[eventName].forEach(listener => {
+    DeviceEventEmitter.removeListener(eventName, listener)
+  });
+  delete listeners[eventName];
+}
 /**
  * Listen for available events
  * @param  {String} eventName Name of event one of connectionSuccess, connectionLost, data, rawData
  * @param  {Function} handler Event handler
  */
 BluetoothSerial.on = (eventName, handler) => {
+  if(!listeners.hasOwnProperty(eventName)) {
+    listeners[eventName] = [];
+  }
+  listeners[eventName].push(handler);
+
   DeviceEventEmitter.addListener(eventName, handler)
 }
 
@@ -19,6 +32,26 @@ BluetoothSerial.on = (eventName, handler) => {
  */
 BluetoothSerial.removeListener = (eventName, handler) => {
   DeviceEventEmitter.removeListener(eventName, handler)
+}
+
+/**
+ * Stop all listening of events
+ */
+BluetoothSerial.removeAllRegisterListener = () => {
+  for(var eventName in listeners) {
+    removeListenerByName(eventName);
+  };
+}
+
+/**
+ * Stop all listening of events
+ */
+BluetoothSerial.removeEventNameListeners = (eventName) => {
+  for(var currentEventName in listeners) {
+    if(eventName === currentEventName) {
+      removeListenerByName(currentEventName);
+    }
+  };
 }
 
 /**
