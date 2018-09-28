aca46a46f694f52ed7cd29e1459118283f2aa9ad
DanielLimaN <daniel.lima.nascimento@gmail.com>
2018-09-28 19:43:00 -0300

    Update RCTBluetoothSerialPackage.java
    Just removed all "@overrides", becouse this was breaking the React Native application. The application acused that the @override was deprecated

--- android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
+++ android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
@@ -13,19 +13,16 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class RCTBluetoothSerialPackage implements ReactPackage {
     static final String TAG = "BluetoothSerial";
 
-    @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
         List<NativeModule> modules = new ArrayList<>();
         modules.add(new RCTBluetoothSerialModule(reactContext));
         return modules;
     }
 
-    @Override
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
 
-    @Override
     public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
         return Collections.emptyList();
     }
