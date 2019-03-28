78daa12fcad3f064b039df11345f4969669a6be3
JoeZhang <z824481406@Gmail.com>
2019-03-28 11:14:09 +0800

    Deprecated createJSModules on Android only for RN >= 0.47

--- android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
+++ android/src/main/java/com/rusel/RCTBluetoothSerial/RCTBluetoothSerialPackage.java
@@ -20,7 +20,7 @@ public class RCTBluetoothSerialPackage implements ReactPackage {
         return modules;
     }
 
-    @Override
+    // Deprecated RN 0.47
     public List<Class<? extends JavaScriptModule>> createJSModules() {
         return Collections.emptyList();
     }
